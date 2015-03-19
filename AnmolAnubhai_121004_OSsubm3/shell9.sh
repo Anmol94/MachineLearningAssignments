77ca249395d65a6bcd9b683280d8116910a54ed0
Anmol94 <anmol.anubhai@gmail.com>
2015-03-19 15:20:31 -0700

    Delete shell9.sh

--- AnmolAnubhai_121004_OSsubm3/shell9.sh
+++
@@ -1,7 +0,0 @@
-echo "Enter the capital of Gujarat beginning with a capital letter"
-read n
-while [ $n != 'Gandhinagar' ]
-do
-echo "Enter the capital of Gujarat beginning with a capital letter"
-read n
-done
